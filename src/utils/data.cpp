758b3a08d55fe27c6989f12d8c9d9251ec574ab3
abb2k <75845401+abb2k@users.noreply.github.com>
2024-10-08 21:57:31 +0300

    test 28 android fix

--- src/utils/data.cpp
+++ src/utils/data.cpp
@@ -948,15 +948,6 @@ int getKey() {
 
 // Function to decode a Base64 string to a byte array
 std::vector<unsigned char> data::base64Decode(const std::string& input) {
-    static const char decodeLookup[] = {
-        62, -1, -1, -1, 63, // +, -, ., /
-        52, 53, 54, 55, 56, 57, 58, 59, 60, 61, // 0-9
-        -1, -1, -1, 0, -1, -1, -1, // :, ;, <, =, >, ?, @
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, // A-Z
-        -1, -1, -1, -1, -1, -1, // [, \, ], ^, _, `
-        26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 // a-z
-    };
-
     std::vector<unsigned char> decodedBytes;
     decodedBytes.reserve((input.length() / 4) * 3);
     uint32_t temp = 0;
